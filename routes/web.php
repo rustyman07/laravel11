34d0b7e483e7fc817f9f76082c89c85a73760041
rustyman <mendozakean12@gmail.com>
2024-09-12 22:18:46 +0800

    routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListingController;
 ;
 
 Route::get('/',[IndexController::class,'index']);
@@ -9,3 +10,5 @@ Route::get('/',[IndexController::class,'index']);
 
 
 Route::get('/test',[IndexController::class,'show']);
+
+Route::resource('Listing',ListingController::class)->only(['index','show']);
